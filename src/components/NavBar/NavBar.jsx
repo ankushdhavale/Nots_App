ff9506fdafa46a15c2ed0283c1de6725d5b6d0d8
Ankush Kailas Dhavale <dhavaleankush7@gmail.com>
2025-07-18 23:54:04 +0530

    build home page

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+export const NavBar = () => {
+    return (
+        <header className="flex gap-3 px-3 py-2 text-2xl border-b border-gray-700 items-center">
+            <div>
+                {/* <img src="" alt="" /> */}Hello
+            </div>
+            <h1>NotesApp</h1>
+        </header>
+    )
+}
