4e5a7c740bbbd39362d167c8ae75bd3efc34c163
Ankush Kailas Dhavale <dhavaleankush7@gmail.com>
2025-07-19 15:48:23 +0530

    build nots logic

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,33 @@
-import React, { useState } from 'react'
-import SideBar from '../SideBar/SideBar';
+import React, { useState } from "react";
+import SideBar from "../SideBar/SideBar";
 
 const Home = () => {
-  const [text, setText] = useState();
-  const [title, setTitle] = useState();
-  console.log(text);
-  
-  return (
-    <div className='flex gap-3'>
-      <SideBar />
-      <div className='flex flex-col items-center w-[90%] mt-4 gap-1'>
-        <input onChange={(e)=>setTitle(e.target.value)} className='w-[40%] py-2 px-3 border border-slate-500/50 rounded outline-none text-xl' type="text" placeholder='Title'/>
-        <textarea onChange={(e)=>setText(e.target.value)} className='w-[40%] h-[40%] p-3 border-slate-500/50 rounded outline-none resize-none' name="" id="" placeholder='Start typing'></textarea>
-      </div>
-    </div>
-  )
-}
+	const [text, setText] = useState("");
+	const [title, setTitle] = useState("");
+	console.log(text);
+
+	return (
+		<div className='flex gap-3'>
+			<SideBar />
+			<div className='flex flex-col items-center w-[90%] mt-4 gap-1'>
+        <input
+          value={title}
+					onChange={(e) => setTitle(e.target.value)}
+					className='w-[40%] py-2 px-3 border border-slate-500/50 rounded outline-none text-xl'
+					type='text'
+					placeholder='Title'
+				/>
+        <textarea
+          value={text}
+					onChange={(e) => setText(e.target.value)}
+					className='w-[40%] h-[40%] p-3 border-slate-500/50 rounded outline-none resize-none'
+					name=''
+					id=''
+					placeholder='Start typing'
+				></textarea>
+			</div>
+		</div>
+	);
+};
 
 export default Home;
---
+++ src/reducers/notsReducers.js
@@ -0,0 +1,6 @@
+export const notsReducers = (state,{type,payload}) => {
+    switch (type) {
+        default:
+            return state;
+    }
+}
